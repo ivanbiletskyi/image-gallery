3e1f4b2cdae3964c68fd517f5a62ab98c9148c6a
Ivan Biletskyi <ivbiletskiy@gmail.com>
2019-04-06 21:02:37 +0300

    server changes

--- server/server.js
+++ server/server.js
@@ -13,6 +13,9 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "static")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
 
 app.get("/api/images", (req, res) => {
   res.send(images);
@@ -41,15 +44,6 @@ app.put("/api/image/:imageId/rate", (req, res) => {
     res.status(400).send("Wrong image rate");
   }
 });
-
-if (process.env.NODE_ENV === "production") {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, "client/build")));
-  // Handle React routing, return all requests to React app
-  // app.get('*', function(req, res) {
-  //   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  // });
-}
 
 app.listen(port, err => {
   if (err) {
