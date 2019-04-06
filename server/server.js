432d8f35abc48fe4411542bcaf77d31e92b9ec4e
Ivan Biletskyi <ivbiletskiy@gmail.com>
2019-04-06 21:13:00 +0300

    production bug fix

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "static")));
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 app.get("/api/images", (req, res) => {
